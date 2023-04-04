49048e2a9c1949360fdc98ae69f0ddd578b925a0
Aleksandr Voron <aleksandr.voron@intel.com>
2023-04-04 13:32:14 +0200

    topk nchw only for arm

--- src/plugins/intel_cpu/src/nodes/topk.cpp
+++ src/plugins/intel_cpu/src/nodes/topk.cpp
@@ -1919,9 +1919,11 @@ void TopK::initSupportedPrimitiveDescriptors() {
 
     std::vector<std::pair<LayoutType, LayoutType>> dataFomats{
         {LayoutType::ncsp, LayoutType::ncsp},
+#if !defined(OPENVINO_ARCH_ARM64)
         {LayoutType::nspc, LayoutType::nspc},
         {LayoutType::nCsp16c, LayoutType::nCsp16c},
         {LayoutType::nCsp8c, LayoutType::nCsp8c}
+#endif
     };
 
     for (const auto &df : dataFomats) {
